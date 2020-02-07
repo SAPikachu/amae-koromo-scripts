ec591955bf3c78b54db8470149d1a22ec3a503d2
SAPikachu <sapikachu@gmail.com>
2020-02-07 14:32:10 +0800

    Avoid hanging

--- logGames.js
+++ logGames.js
@@ -16,16 +16,35 @@ function writeFile (fileNameParts, data) {
 }
 
 async function main () {
+  const deadline = (new Date()).getTime() + 1000 * 110; // 110 seconds
+  let timeoutToken = null;
+  const resetWatchdog = function () {
+    if (timeoutToken) {
+      clearTimeout(timeoutToken);
+    }
+    if ((new Date()).getTime() > deadline) {
+      console.warn("Deadline exceeded");
+      process.exit(1);
+    }
+    timeoutToken = setTimeout(() => {
+      console.warn("Unexpected timeout");
+      process.exit(1);
+    }, 20000);
+  };
+  resetWatchdog();
   const conn = await createMajsoulConnection();
   if (!conn) {
+    clearTimeout(timeoutToken);
     return;
   }
   const liveGames = {};
   try {
     for (const mode of MODES) {
+      resetWatchdog();
       const resp = await conn.rpcCall(".lq.Lobby.fetchGameLiveList", {
         filter_id: mode,
       });
+      console.log(`Mode: ${mode}, Live: ${resp.live_list.length}`);
       for (const game of resp.live_list) {
         liveGames[game.uuid] = true;
         writeFile([
@@ -35,6 +54,7 @@ async function main () {
         ], JSON.stringify(game));
       }
     }
+    const pendingPromises = [];
     const recurseFillData = async function (dir) {
       if (dir !== OUTPUT_DIR && !/^\d+$/.test(path.basename(dir))) {
         return;
@@ -49,38 +69,45 @@ async function main () {
           if (id in liveGames) {
             continue;
           }
+          resetWatchdog();
           const resp = await conn.rpcCall(".lq.Lobby.fetchGameRecord", { game_uuid: id });
           if (!resp.data && !resp.data_url) {
             continue;
           }
-          const recordData = resp.data_url ? await rp({uri: resp.data_url, encoding: null, timeout: 5000}) : resp.data;
-          if (!resp.head || !recordData.length) {
-            continue;
-          }
-          console.log(resp.head.uuid);
-          const game = resp.head;
-          writeFile([
-            "records",
-            game.uuid.split("-")[0],
-            game.config.mode.mode.toString(),
-            (game.config.meta.mode_id || game.config.meta.contest_uid || game.config.meta.room_id).toString(),
-            game.uuid + ".json",
-          ], JSON.stringify(resp.head));
-          writeFile([
-            "records",
-            game.uuid.split("-")[0],
-            game.config.mode.mode.toString(),
-            (game.config.meta.mode_id || game.config.meta.contest_uid || game.config.meta.room_id).toString(),
-            game.uuid + ".recordData",
-          ], recordData);
-          fs.unlinkSync(path.join(dir, ent.name));
+          pendingPromises.push((async function () {
+            const recordData = (resp.data_url && (!resp.data || !resp.data.length)) ? await rp({uri: resp.data_url, encoding: null, timeout: 5000}).catch((e) => console.warn(`Failed to download data for ${id}:`, e)): resp.data;
+            if (!resp.head || !recordData || !recordData.length) {
+              return;
+            }
+            console.log(resp.head.uuid);
+            const game = resp.head;
+            writeFile([
+              "records",
+              game.uuid.split("-")[0],
+              game.config.mode.mode.toString(),
+              (game.config.meta.mode_id || game.config.meta.contest_uid || game.config.meta.room_id).toString(),
+              game.uuid + ".json",
+            ], JSON.stringify(resp.head));
+            writeFile([
+              "records",
+              game.uuid.split("-")[0],
+              game.config.mode.mode.toString(),
+              (game.config.meta.mode_id || game.config.meta.contest_uid || game.config.meta.room_id).toString(),
+              game.uuid + ".recordData",
+            ], recordData);
+            fs.unlinkSync(path.join(dir, ent.name));
+          })());
         }
       }
     };
     await recurseFillData(OUTPUT_DIR);
+    resetWatchdog();
+    await Promise.all(pendingPromises);
   } finally {
     conn.close();
+    clearTimeout(timeoutToken);
   }
+  process.exit(0);
 }
 
 if (require.main === module) {
